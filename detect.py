260dd26f2a8477e312e179749c2257b4267e599f
Rabee <Rabeeekiblawi@gmail.com>
2019-02-15 19:03:50 +0200

    made project ready for api

--- detect.py
+++ detect.py
@@ -15,6 +15,8 @@ import pandas as pd
 import random 
 import pickle as pkl
 import itertools
+import pybase64 as base64
+from flask import Flask, request, json, jsonify, make_response
 
 class test_net(nn.Module):
     def __init__(self, num_layers, input_size):
@@ -28,7 +30,9 @@ class test_net(nn.Module):
         x = x.view(-1)
         fwd = nn.Sequential(self.linear_1, *self.middle, self.output)
         return fwd(x)
-        
+
+num_classes = 80
+app = Flask(__name__, static_url_path='')
 def get_test_input(input_dim, CUDA):
     img = cv2.imread("dog-cycle-car.png")
     img = cv2.resize(img, (input_dim, input_dim)) 
@@ -49,7 +53,6 @@ def arg_parse():
     Parse arguements to the detect module
     
     """
-    
     
     parser = argparse.ArgumentParser(description='YOLO v3 Detection Module')
    
@@ -76,23 +79,15 @@ def arg_parse():
     
     return parser.parse_args()
 
-if __name__ ==  '__main__':
-    args = arg_parse()
-    
-    scales = args.scales
-    
-    
-#        scales = [int(x) for x in scales.split(',')]
-#        
-#        
-#        
+
+def get_response():
+
+#        scales = [int(x) for x in scales.split(',')]    
 #        args.reso = int(args.reso)
 #        
 #        num_boxes = [args.reso//32, args.reso//16, args.reso//8]    
 #        scale_indices = [3*(x**2) for x in num_boxes]
-#        scale_indices = list(itertools.accumulate(scale_indices, lambda x,y : x+y))
-#    
-#        
+#        scale_indices = list(itertools.accumulate(scale_indices, lambda x,y : x+y))       
 #        li = []
 #        i = 0
 #        for scale in scale_indices:        
@@ -100,8 +95,15 @@ if __name__ ==  '__main__':
 #            i = scale
 #        
 #        scale_indices = li
+    request_object = json.loads(request.get_data())
+    image_data = base64.b64decode(request_object['image'])
+    result = []
+    filename = 'image.jpg'
+    file = open(filename, 'wb')
+    file.write(image_data)
 
-    images = args.images
+    images = filename
+    print("IMAGES",images)
     batch_size = int(args.bs)
     confidence = float(args.confidence)
     nms_thesh = float(args.nms_thresh)
@@ -109,14 +111,9 @@ if __name__ ==  '__main__':
 
     CUDA = torch.cuda.is_available()
 
-    num_classes = 80
     classes = load_classes('data/coco.names') 
 
-    #Set up the neural network
-    print("Loading network.....")
-    model = Darknet(args.cfgfile)
-    model.load_weights(args.weightsfile)
-    print("Network successfully loaded")
+   
     
     model.net_info["height"] = args.reso
     inp_dim = int(model.net_info["height"])
@@ -126,7 +123,6 @@ if __name__ ==  '__main__':
     #If there's a GPU availible, put the model on GPU
     if CUDA:
         model.cuda()
-    
     
     #Set the model in evaluation mode
     model.eval()
@@ -138,6 +134,7 @@ if __name__ ==  '__main__':
     except NotADirectoryError:
         imlist = []
         imlist.append(osp.join(osp.realpath('.'), images))
+
     except FileNotFoundError:
         print ("No file or directory with the name {}".format(images))
         exit()
@@ -153,8 +150,6 @@ if __name__ ==  '__main__':
     im_dim_list = [x[2] for x in batches]
     im_dim_list = torch.FloatTensor(im_dim_list).repeat(1,2)
     
-    
-    
     if CUDA:
         im_dim_list = im_dim_list.cuda()
     
@@ -169,10 +164,7 @@ if __name__ ==  '__main__':
         im_batches = [torch.cat((im_batches[i*batch_size : min((i +  1)*batch_size,
                             len(im_batches))]))  for i in range(num_batches)]        
 
-
     i = 0
-    
-
     write = False
     model(get_test_input(inp_dim, CUDA), CUDA)
     
@@ -209,33 +201,46 @@ if __name__ ==  '__main__':
         #loops are slower than vectorised operations. 
         
         prediction = write_results(prediction, confidence, num_classes, nms = True, nms_conf = nms_thesh)
-        
+        # print("PREDICTION",prediction)
+        # # print(prediction[0][1])
+        # # print(prediction[0][2])
+        # # print(prediction[0][3])
+        # # print(prediction[0][4])
+        # x=0
+        # y=0
+        # w=0
+        # h=0
+        #result = []
+        # for i in prediction: 
+        #     x = i[1]
+        #     y = i[2]
+        #     w = i[3]
+        #     h = i[4]            
+        #     object = {
+        #         'x' : str(np.int64(i[1])),
+        #         'y' : str(np.int64(i[2])),
+        #         'w' : str(np.int64(i[3])),
+        #         'h' : str(np.int64(i[4]))
+        #     }
+        #     result.append(object)
+
+        # print(result)
         
         if type(prediction) == int:
             i += 1
             continue
 
         end = time.time()
-        
-                    
+                           
 #        print(end - start)
 
-            
-
-        prediction[:,0] += i*batch_size
-        
-    
-            
-          
+        prediction[:,0] += i*batch_size    
         if not write:
             output = prediction
             write = 1
         else:
             output = torch.cat((output,prediction))
             
-        
-        
-
         for im_num, image in enumerate(imlist[i*batch_size: min((i +  1)*batch_size, len(imlist))]):
             im_id = i*batch_size + im_num
             objs = [classes[int(x[-1])] for x in output if int(x[0]) == im_id]
@@ -262,8 +267,6 @@ if __name__ ==  '__main__':
     output[:,[1,3]] -= (inp_dim - scaling_factor*im_dim_list[:,0].view(-1,1))/2
     output[:,[2,4]] -= (inp_dim - scaling_factor*im_dim_list[:,1].view(-1,1))/2
     
-    
-    
     output[:,1:5] /= scaling_factor
     
     for i in range(output.shape[0]):
@@ -272,15 +275,11 @@ if __name__ ==  '__main__':
         
         
     output_recast = time.time()
-    
-    
     class_load = time.time()
 
     colors = pkl.load(open("pallete", "rb"))
     
-    
     draw = time.time()
-
 
     def write(x, batches, results):
         c1 = tuple(x[1:3].int())
@@ -294,35 +293,37 @@ if __name__ ==  '__main__':
         c2 = c1[0] + t_size[0] + 3, c1[1] + t_size[1] + 4
         cv2.rectangle(img, c1, c2,color, -1)
         cv2.putText(img, label, (c1[0], c1[1] + t_size[1] + 4), cv2.FONT_HERSHEY_PLAIN, 1, [225,255,255], 1)
+        object = {
+            'name' : label,
+            'confidence' : str(np.float(x[5])),
+            'x' : str(np.int64(c1[0])),
+            'y' : str(np.int64(c1[1])),
+            'w' : str(np.int64(c2[0])),
+            'h' : str(np.int64(c2[1]))
+        }
+        result.append(object)
         return img
-    
-            
+
     list(map(lambda x: write(x, im_batches, orig_ims), output))
-      
     det_names = pd.Series(imlist).apply(lambda x: "{}/det_{}".format(args.det,x.split("/")[-1]))
-    
     list(map(cv2.imwrite, det_names, orig_ims))
     
     end = time.time()
-    
-    print()
-    print("SUMMARY")
-    print("----------------------------------------------------------")
-    print("{:25s}: {}".format("Task", "Time Taken (in seconds)"))
-    print()
-    print("{:25s}: {:2.3f}".format("Reading addresses", load_batch - read_dir))
-    print("{:25s}: {:2.3f}".format("Loading batch", start_det_loop - load_batch))
-    print("{:25s}: {:2.3f}".format("Detection (" + str(len(imlist)) +  " images)", output_recast - start_det_loop))
-    print("{:25s}: {:2.3f}".format("Output Processing", class_load - output_recast))
-    print("{:25s}: {:2.3f}".format("Drawing Boxes", end - draw))
-    print("{:25s}: {:2.3f}".format("Average time_per_img", (end - load_batch)/len(imlist)))
-    print("----------------------------------------------------------")
-
-    
+ 
     torch.cuda.empty_cache()
-    
-    
-        
-        
-    
-    
+    return result
+
+#TODO make result local var
+@app.route('/detect', methods=['POST'])
+def detect():
+    result = get_response()
+    return make_response(jsonify(result), 200)
+
+if __name__ ==  '__main__':
+    args = arg_parse()
+     #Set up the neural network
+    print("Loading network.....")
+    model = Darknet(args.cfgfile)
+    model.load_weights(args.weightsfile)
+    print("Network successfully loaded")
+    app.run(host='0.0.0.0', port=8006, threaded=True)
